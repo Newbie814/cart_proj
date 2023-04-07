33dcbd3dd82a131352c2fc3993ec863d9006a84a
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2023-04-06 23:15:51 -0400

    set function for cart item and costs totals

--- src/utilities/utils.js
+++ src/utilities/utils.js
@@ -1,7 +1,11 @@
 export const getCartTotal = (cart) => {
-  console.log(cart);
   let totalAmount = 0;
   let totalCost = 0;
+
+  for (let { amount, price } of cart.values()) {
+    totalAmount += amount;
+    totalCost += amount * price;
+  }
 
   return { totalAmount, totalCost };
 };
